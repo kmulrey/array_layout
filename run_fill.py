357b243e9f8a3d9570e10398c4530e71ca896d95
Katie <kmurley@gmail.com>
2021-02-02 20:35:23 -0500

    .

--- run_fill.py
+++ run_fill.py
@@ -16,45 +16,46 @@ parser.add_option("-e", "--energybin", default = "6", help = "energybin")
 parser.add_option("-t", "--type", default = "proton", help = "type")
 parser.add_option("-z", "--zenithbin", default = "0", help = "zenithbin")
 parser.add_option("-d", "--detectorfile", default = "test_layout.txt", help = "detector file")
+#parser.add_option("-m", "--threshold", default = 1.0, help = "threshold")
+#parser.add_option("-c", "--trigger_condition", default = 3, help = "trigger condition")
+parser.add_option("-r", "--radius", default = 250.0, help = "max radius")
+parser.add_option("-n", "--trials", default = 100, help = "number of cores per shower")
+
 
 (options, args) = parser.parse_args()
 energy = str(options.energybin)
 typeN = str(options.type)
 zenith = str(options.zenithbin)
 det_file = str(options.detectorfile)
+#thresh = float(options.threshold)
+#trigger_condition = int(options.trigger_condition)
+maxR = float(options.radius)
+nTrials = int(options.trials)
 
 ########################################
 array_dir='/vol/astro2/users/kmulrey/array_layout/layout/'
 geant_directory='/vol/astro3/lofar/sim/kmulrey/lora/flux/all_information2/'
 output_directory='/vol/astro7/lofar/kmulrey/array_design/'
+em_peak=6.0
+
 print(energy)
+outdir=output_directory+det_file+'_'+str(int(maxR))+'/'
+import os
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+    
 
-
-
-detectors=np.genfromtxt(array_dir+det_file,skip_header=1)
+detectors=np.genfromtxt(array_dir+det_file+'.txt',skip_header=1)
 nDet=len(detectors)
 
-#typeN='proton'
-#energy='6'
 
-
-#zenith='0'
 
 geantDir=geant_directory+typeN+'/'+energy+'/'+zenith+'/geant/'
 print(geantDir)
 print(array_dir+det_file)
 
-'''
-thresh=1.0
-trigger_condition=3
-em_peak=6.0
 
-maxR=250
-nTrials=100
-
-geantDir=geant_directory+typeN+'/'+energy+'/'+zenith+'/geant/'
 files=glob.glob(geantDir+'*.geant')
 
 core=np.zeros([nTrials,2])
 event_info=np.zeros([nTrials,nDet,6])
-'''
